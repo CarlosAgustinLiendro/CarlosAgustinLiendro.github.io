79459d6413aa3f938179c51a69b930b948f0a857
CarlosAgustinLiendro <117772359+CarlosAgustinLiendro@users.noreply.github.com>
2023-01-29 14:55:29 -0300

    Creacion del proyecto

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.getElementById("micv").addEventListener("click", cambio)
+        function cambio() {
+            document.getElementById("micv").innerHTML = "Muchas gracias";
+            document.getElementById("micv").style.backgroundColor = "#181D3A";
+        }
+
+document.getElementById("opciones").addEventListener("click", colordefondo)
+        function colordefondo() {
+            document.getElementById("opciones").style.backgroundColor = "#E91E1E";
+        }
